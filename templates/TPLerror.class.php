b9d36d23001e96ca5ce08448fe90441073218e83
HristoGeorgiev12 <christiangg.bg@gmail.com>
2015-08-18 08:41:27 +0300

    Add .gitignore

---
+++ templates/TPLerror.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Georgievi
+ * Date: 18.8.2015 ã.
+ * Time: 08:36 ÷.
+ */
+
+class TPLerror extends Template {
+    public function Body() {
+        ?>
+            <p>Error message.</p>
+        <?php
+    }
+}
