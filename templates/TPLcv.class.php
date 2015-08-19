b28e6c9d54e096d63649d1ad94fb4a4b9538d0e0
HristoGeorgiev12 <christiangg.bg@gmail.com>
2015-08-19 10:13:56 +0300

    Add .gitignore

--- templates/TPLcv.class.php
+++ templates/TPLcv.class.php
@@ -9,20 +9,53 @@
 class TPLcv extends Template {
     protected $table = 'comments';
 
+    private function insertParams() {
+        //Array to be inserted into DB;
+        $insertParams = array();
+
+        //Names
+        $insertParams['fname'] = $this->aParam['fname'];
+        $insertParams['mname'] = $this->aParam['mname'];
+        $insertParams['lname'] = $this->aParam['lname'];
+
+        //Contacts;
+        $insertParams['email'] = $this->aParam['email'];
+        $insertParams['phoneNumber'] = $this->aParam['phoneNumber'];
+
+        //BirthDate and Nationality;
+        $insertParams['dateOfBirth'] = $this->aParam['dateOfBirth'];
+        $insertParams['nationality'] = $this->aParam['nationality'];
+
+
+        return $insertParams;
+
+    }
+
     protected function Title() {
         return "Home";
     }
 
     protected function Body() {
-        $connect = new Connect('pdo','comments');
-        $result = $connect->connect->prepare('Select * FROM '.$connect->table);
-        $result->execute();
-        $all = $result->fetchAll();
+        //Implode keys and values;
+        $keyParam = implode(', ', array_keys($this->insertParams()));
 
-//        var_dump($all);
+        $valueConcatArray = array();
+        foreach($this->insertParams() as $key=>$value) {
+            $valueConcatArray[] = " :".$value ;
+        }
+        $valueParam = implode(',', $valueConcatArray);
 
+        //Insert into DB personal data;
+        $db = new Connect('documenti','personalData');
+        $connect = $db->connect->prepare("INSERT INTO "
+                                            .$this->table." (".$keyParam.")
+												VALUES(".$valueParam.")");
 
-        echo $this->getParam('name');
+        $connect->execute($this->insertParams());
+
+        var_dump($this->insertParams());
+        var_dump($keyParam);
+        var_dump($valueParam);
 
         ?>
         <form action="" method="post">
@@ -31,37 +64,41 @@ class TPLcv extends Template {
                 Име<input type="text"
                           name="fname"
                           pattern="[А-Яа-я]{2,12}"
-                          title="използвайте само Кирилица!"
+                          title="Използвайте само Кирилица!"
                           required><br>
 
                 Презиме<input type="text"
                               name="mname"
                               pattern="[А-Яа-я]{2,12}"
-                              title="използвайте само Кирилица!"
+                              title="Използвайте само Кирилица!"
                               required><br>
 
                 Фамилия<input type="text"
                               name="lname"
                               pattern="[А-Яа-я]{2,12}"
-                              title="използвайте само Кирилица!"
-                              required><br>
+                              title="Използвайте само Кирилица!"
+                              required><br><hr>
 
                 Електронна поща<input type="email"
-                                      title="Wrong email"
+                                      title="Грешна форма на email формата."
                                       name="email"
                                       required><br>
 
                 Телефонен номер<input type="number"
-                                      name="phoneNumeber"
+                                      name="phoneNumber"
+                                      title="Въведете телефонен номер."
+                                      required><br><hr>
+
+                Национаност<input type="text"
+                                      name="nationality"
+                                      title="Въведете националноста си."
                                       required><br>
 
                 Дата на раждане<input type="date"
                                       name="dateOfBirth"
-                                      required><br>
-            </div>
-            <h3>Образование</h3>
-            <div id="SchoolHistory">
-                <input type="">
+                                      title="Изберете рожденната си дата."
+                                      required><br><hr>
+
             </div>
             <input type="submit" name="submit">
         </form>
