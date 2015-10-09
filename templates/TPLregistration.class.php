2a9f5ce39ac42a94b81da5af1db34123998aa95b
HristoGeorgiev12 <christiangg.bg@gmail.com>
2015-10-09 20:31:03 +0300

    Update registration form and performance

--- templates/TPLregistration.class.php
+++ templates/TPLregistration.class.php
@@ -8,20 +8,30 @@
 
 class TPLregistration extends Template {
     protected function insertRegistrationArray() {
-        $insertParams = array();
         if(isset($_POST['submitRegistration'])) {
             //If the passwords match, continue with the registration;
             if($this->aParam['userPassword'] == $this->aParam['userPasswordConfirm']) {
 
-            $insertParams['userEmail'] = $this->aParam['userEmail'];
-            $insertParams['userPassword'] = md5($this->aParam['userPassword']);
+                $insertParams = array();
+                $insertParams['userEmail'] = $this->aParam['userEmail'];
+                $insertParams['userPassword'] = md5($this->aParam['userPassword']);
+
+                //check is the Email in users DB
+                    //if true, don`t insert
+                    //else insert
+                $emailCheck = $this->select('documenti','users',$insertParams);
+                if(empty($emailCheck)) {
+                    //insert into DB;
+                    $this->insert('documenti','users',$insertParams);
+                    return 'Вие успешно създадохте своя профил!';
+                }else {
+                    return "Вече има създаден профил с този Email адрес.";
+                }
 
 
-            $this->insert('documenti','users',$insertParams);
-            return 'Successfully created account';
+            }else {
+                return "Въведениете от вас пароли не съвпадат.";
             }
-        }else {
-            return "Passwords did not match";
         }
     }
 
@@ -30,11 +40,20 @@ class TPLregistration extends Template {
     }
 
     public function Body() {
-        //execute insert method and return message;
-        echo $this->insertRegistrationArray();
+        ?>
+            <div id="registrationForm">
+        <?php
+//        if(isset($_POST['submitRegistration'])) {
+            //execute insert method and return message;
+            echo $this->insertRegistrationArray();
+//        }
 
         //registration form;
         $this->registration();
+
+        ?>
+            </div>
+        <?php
     }
 
 }
